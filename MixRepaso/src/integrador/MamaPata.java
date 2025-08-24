29b1b0d8043edc0e78b3b4ec9de2a0e3e2ea75f3
Matt <matiascorrea@protonmail.com>
2025-08-24 17:59:27 -0300

    Actualización
    Subí clase MamaPata

---
+++ MixRepaso/src/integrador/MamaPata.java
@@ -0,0 +1,15 @@
+
+package integrador;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Cetera Evelyn
+ */
+public interface MamaPata {
+    public static final double PICO = 6.8;
+    public static final double PATAS = 7.5;
+
+    void tocarOrgano(Scanner sc);
+}
